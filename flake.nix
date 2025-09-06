0d55be6f3dcec9b9ee17e7016928505c33b7329d
nikiaker <md.macek@gmail.com>
2025-09-06 23:12:57 +0200

    new package vita3k

--- flake.nix
+++ flake.nix
@@ -6,15 +6,22 @@
       url = "git+ssh://git@github.com/Nikiaker/nikiaker-hello-world?ref=main";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    vita3k = {
+      url = "git+ssh://git@github.com/Nikiaker/vita3k-flake?ref=main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: 
+  outputs = { self, ... }@inputs: 
   let 
     system = "x86_64-linux";
   in 
   {
-    packages.${system}.nikiaker-hello-world = inputs.nikiaker-hello-world.nikiaker-hello-world;
-    
-    nikiaker-hello-world = self.packages.${system}.nikiaker-hello-world;
+    nikiaker-hello-world = inputs.nikiaker-hello-world.nikiaker-hello-world;
+    vita3k = inputs.vita3k.vita3k;
+
+    packages.${system}.nikiaker-hello-world = self.nikiaker-hello-world;
+    packages.${system}.vita3k = self.vita3k;
   };
 }
